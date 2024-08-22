cdabad125de6896d5f8c029d0a212ffb8148fe47
tuyishimehono <tuhonori1@gmail.com>
2024-08-22 12:10:54 +0200

    Improve Exercise6

--- CodeImprovements/Exercise6.js
+++ CodeImprovements/Exercise6.js
@@ -1,11 +1,5 @@
-function maxNumber(a, b, c) {
-  if (a > b && a > c) {
-    return a;
-  } else {
-    if (b > a && b > c) {
-      return b;
-    } else {
-      return c;
-    }
-  }
-}
+const maxNumber = (a, b, c) => {
+  if (typeof a !== "number" || typeof b !== "number" || typeof c !== "number")
+    throw new Error("Invalid input");
+  return Math.max(a, b, c);
+};
